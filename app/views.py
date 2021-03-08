4f1ce5c1fcf7e728a7fe79ac13d230b3ac4c02f5
Ashley-Ann Ellison <aashleyellison51@gmail.com>
2021-03-07 19:11:12 -0500

    Final Edit

--- app/views.py
+++ app/views.py
@@ -45,7 +45,7 @@ def login():
         #if form.username.data:
             # Get the username and password values from the form.
         username = form.username.data
-        password = form.username.data
+        password = form.password.data
 
             # using your model, query database for a user based on the username
             # and password submitted. Remember you need to compare the password hash.
